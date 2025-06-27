c26a72738119449c9d82db50d5ba30d6ba154740
Rohan <Rohan.Singh@RaikSolutions.com>
2025-06-27 09:34:11 +0530

    first commit

---
+++ FileTransferUtility/FtpService.cs
@@ -0,0 +1,60 @@
+﻿using FluentFTP;
+
+public class FTPService : IDisposable
+{
+    private readonly FtpClient? _ftpClient;
+    private bool disposedValue;
+
+    public FTPService(string Host, string UserName, string Password)
+    {
+        try
+        {
+            _ftpClient = new FtpClient(Host, UserName, Password);
+            _ftpClient.Connect();
+            Console.WriteLine("FTP Client connected successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+
+   
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _ftpClient?.Disconnect();
+                _ftpClient?.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+
+    public void UploadFiles(string inputFolderPath, string targetFolderPath)
+    {
+        try
+        {
+            foreach (string filePath in Directory.GetFiles(inputFolderPath))
+            {
+              _ftpClient?.UploadFile(filePath, targetFolderPath);
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
---
+++ FileTransferUtility/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string host = "ftp.example.com";
+string username = "your-ftp-username";
+string password = "your-ftp-password";
+string localFolderPath = @"C:\MyLocalFiles";
+string remoteFolderPath = "/remote/target/folder";
+
+try
+{
+    using (var ftpService = new FTPService(host, username, password))
+    {
+        ftpService.UploadFiles(localFolderPath, remoteFolderPath);
+        Console.WriteLine("Files uploaded successfully.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred during FTP upload:");
+    Console.WriteLine(ex.Message);
+}
